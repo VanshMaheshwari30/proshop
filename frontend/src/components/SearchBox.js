0d35de50aec33ec8cbb1ecd41a5ee09da678e541
VanshMaheshwari30 <vansh.maheshwari30@gmail.com>
2022-06-09 03:07:49 +0530

    Second Commit

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import { Form, Button } from 'react-bootstrap' 
+
+const SearchBox = ({history}) => {
+
+    const [keyword, setKeyword] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if(keyword.trim()) {
+            history.push(`/search/${keyword}`)
+        } else {
+            history.push('/')
+        }
+    }
+
+  return (
+    <Form
+      onSubmit={submitHandler}
+      style={{ display: 'flex' }}
+      className='frmctrl'
+    >
+      <Form.Control
+        type='text'
+        name='q'
+        onChange={(e) => {
+          setKeyword(e.target.value)
+        }}
+        placeholder='Search Products....'
+        className='ms-sm-2 me-sm-3 frminpt'
+      ></Form.Control>
+      <Button
+        type='submit'
+        variant='outline-success'
+        className='py-2 px-3 frmbtn'
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='currentColor'
+          className='bi bi-search'
+          viewBox='0 0 16 16'
+          width='16'
+          height='16'
+        >
+          <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+        </svg>
+      </Button>
+    </Form>
+  )
+}
+
+export default SearchBox
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,28 +5,54 @@ import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import {listProductDetails} from '../actions/productActions';
+import {listProductDetails, createProductReview} from '../actions/productActions';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
+import Meta from '../components/Meta';
 
 const ProductScreen = ({history, match}) => {
 
     const [qty, setQty] = useState(1)
+    const [rating, setRating] = useState(0)
+    const [comment, setComment] = useState('')
 
     const dispatch = useDispatch()
     const productDetails = useSelector((state) => state.productDetails)
     const {loading, error, product} = productDetails
 
+    const userLogin = useSelector((state) => state.userLogin)
+    const {userInfo} = userLogin
+
+    const productReviewCreate = useSelector((state) => state.productReviewCreate)
+    const {success: successProductReview, error: errorProductReview} = productReviewCreate
+
     useEffect(() => {
+        if(successProductReview) {
+            alert('Review Submitted!')
+            setRating(0)
+            setComment('')
+            dispatch({type: PRODUCT_CREATE_REVIEW_RESET})
+        }
         dispatch(listProductDetails(match.params.id))
-      }, [dispatch,match])
+      }, [dispatch, match, successProductReview])
 
     const addToCartHandler = () => {
         history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(createProductReview(match.params.id, {
+            rating,
+            comment
+        }))
     }
 
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>Go Back</Link>
             {loading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>) : (
+                <>
+                <Meta title={product.name}/>
                 <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid />
@@ -55,8 +81,8 @@ const ProductScreen = ({history, match}) => {
                                     <Col>
                                         Price:
                                     </Col>
-                                    <Col>
-                                        <strong>${product.price}</strong>
+                                    <Col style={{ fontWeight: 'bold' }}>
+                                        <strong>$ {product.price}</strong>
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
@@ -74,7 +100,7 @@ const ProductScreen = ({history, match}) => {
                             {product.countInStock > 0 && (
                                 <ListGroup.Item>
                                     <Row>
-                                        <Col>Qty</Col>
+                                        <Col className='d-flex align-items-center'>Quantity: </Col>
                                         <Col>
                                             <Form.Control as='select' value={qty} onChange={(e) =>
                                             setQty(e.target.value)}>
@@ -90,16 +116,59 @@ const ProductScreen = ({history, match}) => {
                             )}
 
                             <ListGroup.Item>
-                                <Button onClick={addToCartHandler} className='btn-block' type='button' disabled={product.countInStock === 0}>
+                            <div className='d-grid gap-2'>
+                                <Button onClick={addToCartHandler} className='btn btn-primary' type='button' disabled={product.countInStock === 0}>
                                     ADD TO CART
-                                </Button> 
+                                </Button>
+                            </div> 
                             </ListGroup.Item>
                         </ListGroup>
                     </Card>
                 </Col>
             </Row>
+            <Row>
+                <Col md={6}>
+                    <h2 className='mt-3'>Reviews</h2>
+                    {product.reviews.length === 0 && <Message>No Reviews</Message>}
+                    <ListGroup variant='flush'>
+                        {product.reviews.map(review => (
+                            <ListGroup.Item key={review._id}>
+                                <strong style={{ fontWeight: '900' }}>{review.name}</strong>
+                                <Rating value={review.rating} />
+                                <p>{review.createdAt.substring(0, 10)}</p>
+                                <p>{review.comment}</p>
+                            </ListGroup.Item>
+                        ))}
+                        <ListGroup.Item className='ps-0'>
+                            <h2>Write a Customer Review</h2>
+                            {errorProductReview && <Message variant='danger'>{errorProductReview}</Message>}
+                            {userInfo ? (
+                                <Form onSubmit={submitHandler}>
+                                    <Form.Group controlId='rating' className='my-2'>
+                                        <Form.Label>Rating</Form.Label>
+                                        <Form.Control as='select' value={rating} onChange={
+                                        (e) => setRating(e.target.value)}>
+                                            <option value=''>Select...</option>
+                                            <option value='1'>1 - Poor</option>
+                                            <option value='2'>2 - Fair</option>
+                                            <option value='3'>3 - Good</option>
+                                            <option value='4'>4 - Very Good</option>
+                                            <option value='5'>5 - Excellent</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group controlId='comment' className='my-2'>
+                                        <Form.Label>Comment</Form.Label>
+                                        <Form.Control as='textarea' row='3' value={comment} onChange={(e) => setComment(e.target.value)}></Form.Control>
+                                    </Form.Group>
+                                    <Button type='submit' variant='primary'>Submit</Button>
+                                </Form>
+                            ) : <Message>Please <Link to='/login' style={{ textDecoration: 'none', fontWeight: 'bold' }}>sign in</Link> to write a review {' '}</Message>}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+            </>
             )}
-            
         </>
     )
 }
